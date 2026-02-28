65740e15af133b3f5d5f892444f6e61d6b91901f
Sandeep123karan <sandeepkaran234@gmail.com>
2026-02-28 13:34:40 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,48 +1,50 @@
+
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const adminSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-
-    role: {
-      type: String,
-      enum: ["admin"],
-      default: "admin",
-    },
+{
+  name: {
+    type: String,
+    required: true,
+    trim: true
   },
-  { timestamps: true }
-);
 
-/* 🔐 HASH PASSWORD */
-adminSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+
+  role: {
+    type: String,
+    enum: ["admin"],
+    default: "admin"
+  }
+
+}, { timestamps: true });
+
+/* 🔐 HASH PASSWORD BEFORE SAVE */
+adminSchema.pre("save", async function () {
+
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
+
 });
 
 /* 🔐 MATCH PASSWORD */
 adminSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  return bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model("Admin", adminSchema);
