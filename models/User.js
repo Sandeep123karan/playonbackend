72b2733fc5f57612a3ae7d56f91a6b1c2ed567cf
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-02 12:46:04 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,42 @@
+
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+
+  phone: {
+    type: String,
+    default: ""
+  },
+
+  dob: {
+    type: Date
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+}, { timestamps: true });
+
+
+// 🔐 hash password
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+module.exports = mongoose.model("User", userSchema);
