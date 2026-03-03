548810db0585b96332c0a2ee79c61b8b600b60bc
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-03 13:20:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-
-// const app = express();
-// const bannerRoutes = require("./routes/bannerRoutes");
-// const scheduleRoutes = require("./routes/scheduleRoutes");
-// const highlightRoutes = require("./routes/highlightRoutes");
-// const liveTvRoutes = require("./routes/liveTvRoutes");
-// const prematchRoutes = require("./routes/prematchRoutes");
-// const userRoutes = require("./routes/userRoutes");
-// const planRoutes = require("./routes/planRoutes");
-
-
-// app.use(cors());
-// app.use(express.json());
-
-// mongoose.connect(process.env.MONGO_URL)
-// .then(()=>console.log("🔥 MongoDB Connected"))
-// .catch(err=>console.log("Mongo Error:",err));
-
-// app.use("/api/admin", require("./routes/adminAuthRoutes"));
-// app.use("/api/banners", bannerRoutes);
-// app.use("/api/schedule", scheduleRoutes);
-// app.use("/api/highlight", highlightRoutes);
-// app.use("/api/live-tv", liveTvRoutes);
-// app.use("/api/prematch", prematchRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/plans", planRoutes);          // 🔥 plans
-// app.use("/api/subscription", require("./routes/subscriptionRoutes"));
-// app.use("/api/payment", require("./routes/paymentRoutes"));
-
-
-
-// app.listen(process.env.PORT || 9000, ()=>{
-//   console.log("Server running 🔥");
-// });
 require("dotenv").config();
 
 const express = require("express");
@@ -44,8 +5,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
-/* ================== ROUTES IMPORT ================== */
 const bannerRoutes = require("./routes/bannerRoutes");
 const scheduleRoutes = require("./routes/scheduleRoutes");
 const highlightRoutes = require("./routes/highlightRoutes");
@@ -53,34 +12,16 @@ const liveTvRoutes = require("./routes/liveTvRoutes");
 const prematchRoutes = require("./routes/prematchRoutes");
 const userRoutes = require("./routes/userRoutes");
 const planRoutes = require("./routes/planRoutes");
+const tournamentRoutes = require("./routes/tournamentRoutes");
 
-/* ================== CORS SETUP ================== */
-const allowedOrigins = [
-  "https://antiquewhite-coyote-117976.hostingersite.com", // 🔥 tera frontend domain
-  "http://localhost:3000",
-  "http://localhost:5173"
-];
 
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); // postman allow
-
-    if(allowedOrigins.indexOf(origin) === -1){
-      return callback(new Error("CORS not allowed"), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
-/* ================== MONGODB ================== */
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("🔥 MongoDB Connected"))
 .catch(err=>console.log("Mongo Error:",err));
 
-/* ================== ROUTES ================== */
 app.use("/api/admin", require("./routes/adminAuthRoutes"));
 app.use("/api/banners", bannerRoutes);
 app.use("/api/schedule", scheduleRoutes);
@@ -88,11 +29,13 @@ app.use("/api/highlight", highlightRoutes);
 app.use("/api/live-tv", liveTvRoutes);
 app.use("/api/prematch", prematchRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/plans", planRoutes);
+app.use("/api/plans", planRoutes);          // 🔥 plans
 app.use("/api/subscription", require("./routes/subscriptionRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes"));
+app.use("/api/tournaments", tournamentRoutes);
 
-/* ================== SERVER ================== */
+
+
 app.listen(process.env.PORT || 9000, ()=>{
-  console.log("🚀 Server running 🔥");
+  console.log("Server running 🔥");
 });
