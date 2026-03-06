9a5178ed69078c95e235ccfc54a0836cbcae848e
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-06 12:30:33 +0530

    Update matchRoutes.js

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
+
 const matchController = require("../controllers/matchController");
+
 const { protectAdmin, adminOnly } = require("../middleware/authMiddleware");
 
+
+// Public
 router.get("/", matchController.getAllMatches);
+router.get("/tournament/:tournamentId", matchController.getMatchesByTournament);
 router.get("/:id", matchController.getSingleMatch);
 
+
+// Admin
 router.post("/add", protectAdmin, adminOnly, matchController.addMatch);
 router.put("/:id", protectAdmin, adminOnly, matchController.updateMatch);
 router.delete("/:id", protectAdmin, adminOnly, matchController.deleteMatch);
+
 
 module.exports = router;
