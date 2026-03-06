8fcab69b4aa057c1d4923aabbf6f3e0b6cafcaa2
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-06 17:44:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const userRoutes = require("./routes/userRoutes");
 const planRoutes = require("./routes/planRoutes");
 const tournamentRoutes = require("./routes/tournamentRoutes");
 const matchRoutes = require("./routes/matchRoutes")
+const categoryRoutes = require("./routes/categoryRoutes");
 
 
 app.use(cors());
@@ -37,6 +38,7 @@ app.use("/api/tournaments", tournamentRoutes);
 app.use("/api/matches", matchRoutes);
 app.use("/api/premium", require("./routes/premiumContentRoutes"));
 app.use("/api/highlight-category", require("./routes/highlightCategoryRoutes"));
+app.use("/api/categories", categoryRoutes);
 
 
 app.listen(process.env.PORT || 9000, ()=>{
