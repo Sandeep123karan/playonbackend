161a334cf7c1edbfd296d1c70ce4fb69c4fa2fc5
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-10 15:39:21 +0530

    Update scheduleRoutes.js

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -3,13 +3,44 @@ const router = express.Router();
 
 const scheduleCtrl = require("../controllers/scheduleController");
 const { protectAdmin } = require("../middleware/authMiddleware");
+const upload = require("../middleware/upload");
 
-/* ADMIN */
-router.post("/add", scheduleCtrl.addSchedule);
-router.put("/update/:id", scheduleCtrl.updateSchedule);
-router.delete("/delete/:id",  scheduleCtrl.deleteSchedule);
+/* ================= ADMIN ================= */
 
-/* PUBLIC */
+// upload images
+const cpUpload = upload.fields([
+  { name: "leagueLogo", maxCount: 1 },
+  { name: "team1Logo", maxCount: 1 },
+  { name: "team2Logo", maxCount: 1 },
+  { name: "bannerImage", maxCount: 1 }
+]);
+
+// add schedule
+router.post(
+  "/add",
+  protectAdmin,
+  cpUpload,
+  scheduleCtrl.addSchedule
+);
+
+// update schedule
+router.put(
+  "/update/:id",
+  protectAdmin,
+  cpUpload,
+  scheduleCtrl.updateSchedule
+);
+
+// delete
+router.delete(
+  "/delete/:id",
+  protectAdmin,
+  scheduleCtrl.deleteSchedule
+);
+
+
+/* ================= PUBLIC ================= */
+
 router.get("/all", scheduleCtrl.getSchedule);
 router.get("/:id", scheduleCtrl.getSingleSchedule);
 
