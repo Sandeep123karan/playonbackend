861a68a6c1aa98881f787b5130fbdf4064c65bfc
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-10 15:14:04 +0530

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,12 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 const categoryController = require("../controllers/categoryController");
-
 const { protectAdmin, adminOnly } = require("../middleware/authMiddleware");
+const upload = require("../middleware/upload");
 
 /* ADMIN */
-router.post("/add", protectAdmin, adminOnly, categoryController.addCategory);
-router.put("/update/:id", protectAdmin, adminOnly, categoryController.updateCategory);
+router.post(
+  "/add",
+  protectAdmin,
+  adminOnly,
+  upload.fields([
+    { name: "icon", maxCount: 1 },
+    { name: "image", maxCount: 1 }
+  ]),
+  categoryController.addCategory
+);
+
+router.put(
+  "/update/:id",
+  protectAdmin,
+  adminOnly,
+  upload.fields([
+    { name: "icon", maxCount: 1 },
+    { name: "image", maxCount: 1 }
+  ]),
+  categoryController.updateCategory
+);
+
 router.delete("/delete/:id", protectAdmin, adminOnly, categoryController.deleteCategory);
 
 /* PUBLIC */
