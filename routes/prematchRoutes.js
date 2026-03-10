9a670f0a9c83a92fef7d03a45aee941442c83e82
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-10 16:46:58 +0530

    Update prematchRoutes.js

--- routes/prematchRoutes.js
+++ routes/prematchRoutes.js
@@ -1,3 +1,47 @@
+// const express = require("express");
+// const router = express.Router();
+// const upload = require("../middleware/upload");
+
+// const {
+//   createPrematch,
+//   getAllPrematch,
+//   getSinglePrematch,
+//   updatePrematch,
+//   deletePrematch,
+//   toggleLive
+// } = require("../controllers/prematchController");
+
+// const { protectAdmin, adminOnly } = require("../middleware/authMiddleware");
+
+// /* upload fields */
+// const cpUpload = upload.fields([
+//   { name: "image", maxCount: 1 },
+//   { name: "team1Logo", maxCount: 1 },
+//   { name: "team2Logo", maxCount: 1 }
+// ]);
+
+// // admin create
+// router.post("/create", protectAdmin, adminOnly, cpUpload, createPrematch);
+
+// // get all
+// router.get("/all", getAllPrematch);
+
+// // single
+// router.get("/:id", getSinglePrematch);
+
+// // update
+// router.put("/update/:id", protectAdmin, adminOnly, cpUpload, updatePrematch);
+
+// // delete
+// router.delete("/delete/:id", protectAdmin, adminOnly, deletePrematch);
+
+// // live toggle
+// router.put("/live/:id", protectAdmin, adminOnly, toggleLive);
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
@@ -8,7 +52,8 @@ const {
   getSinglePrematch,
   updatePrematch,
   deletePrematch,
-  toggleLive
+  toggleLive,
+  getMatchesByCategory
 } = require("../controllers/prematchController");
 
 const { protectAdmin, adminOnly } = require("../middleware/authMiddleware");
@@ -25,6 +70,9 @@ router.post("/create", protectAdmin, adminOnly, cpUpload, createPrematch);
 
 // get all
 router.get("/all", getAllPrematch);
+
+// category wise matches
+router.get("/category/:categoryId", getMatchesByCategory);
 
 // single
 router.get("/:id", getSinglePrematch);
