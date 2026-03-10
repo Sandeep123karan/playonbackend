bfd87f365c760b6358e9f14f9801b0684c134b8f
Sandeep123karan <sandeepkaran234@gmail.com>
2026-03-10 14:19:00 +0530

    Update premiumContentRoutes.js

--- routes/premiumContentRoutes.js
+++ routes/premiumContentRoutes.js
@@ -6,42 +6,47 @@ const {
   adminOnly,
 } = require("../middleware/authMiddleware");
 
+const upload = require("../middleware/upload"); // 👈 important
+
+
 /* ===============================
    ADD PREMIUM CONTENT
-   (ADMIN TOKEN REQUIRED)
 ================================ */
 router.post(
   "/add",
   protectAdmin,
   adminOnly,
+  upload.single("image"),   // 👈 image upload
   premiumCtrl.addPremiumContent
 );
 
+
 /* ===============================
    GET ALL PREMIUM CONTENT
-   (PUBLIC)
 ================================ */
 router.get("/", premiumCtrl.getAllPremiumContent);
+
 
 /* ===============================
    GET SINGLE PREMIUM CONTENT
 ================================ */
 router.get("/:id", premiumCtrl.getPremiumContentById);
 
+
 /* ===============================
    UPDATE PREMIUM CONTENT
-   (ADMIN TOKEN REQUIRED)
 ================================ */
 router.put(
   "/:id",
   protectAdmin,
   adminOnly,
+  upload.single("image"),   // 👈 image update
   premiumCtrl.updatePremiumContent
 );
 
+
 /* ===============================
    DELETE PREMIUM CONTENT
-   (ADMIN TOKEN REQUIRED)
 ================================ */
 router.delete(
   "/:id",
